d88bd30a179ce5fa35ad1b8e592df232ffde35c5
Mateo Cañavera <mcanavera@unal.edu.co>
2022-04-27 07:33:31 -0500

    Update zoologico.py

--- gestion/zoologico.py
+++ gestion/zoologico.py
@@ -1,36 +1,25 @@
 from gestion.zona import Zona
 class Zoologico:
-    def __init__ (self,nombre=None,ubicacion=None):
-
-        
-        self._nombre=nombre
-        self._ubicacion= ubicacion
-        self._zonas=[]
-    def agregarZonas(self,zonas):
-
-        self._zonas.append(zonas)
-
-
-
-    def cantidadTotalAnimales(self):
-        
-        Nzonas=0
-        for i in self._zonas:
-            Nzonas+=i.cantidadAnimales()
-        return Nzonas
-      
-    
-    def getNombre(self):
-
-        return self._nombre
-
-    def setNombre(self,nombre):
-
-        self._nombre=nombre
-
-    def getUbicacion(self):
-         return self._ubicacion
-    
-    def getZona(self):
-        return self._zonas
-        
+  def __init__(self,nombre=None,ubicacion=None):
+    self._nombre=nombre
+    self._ubicacion=ubicacion
+    self._zonas=[]
+  def setNombre(self,nombre):
+    self._nombre=nombre
+  def setUbicacion(self,ubicacion):
+    self._ubicacion=ubicacion
+  def setZonas(self,zonas):
+    self._zonas=zonas
+  def getNombre(self):
+    return self._nombre
+  def getUbicacion(self):
+    return self._ubicacion
+  def getZona(self):
+    return self._zonas
+  def agregarZonas(self,zona):
+    self._zonas.append(zona)
+  def cantidadTotalAnimales(self):
+    x=0
+    for i in self._zonas:
+      x+=i.cantidadAnimales()
+    return x
