bdd20c653bdb032c8ac918c49feaa2144542923e
Mateo Cañavera <mcanavera@unal.edu.co>
2022-04-27 06:59:50 -0500

    f

---
+++ .github/classroom/gestion/zoologico.py
@@ -0,0 +1,33 @@
+from gestion.zona import Zona
+class Zoologico:
+    def __init__ (self,nombre=None,ubicacion=None):
+
+        
+        self._nombre=nombre
+        self._ubicacion= ubicacion
+        self._zonas=None
+
+    def agregarZonas(self,zonas):
+        self._zona=[]
+        self._zona.append(zonas)
+
+
+
+    def cantidadTotalAnimales(self):
+        Nzonas=0
+        for i in self._zonas():
+            Nzonas+=i.cantidadAnimales
+        return Nzonas
+      
+    
+    def getNombre(self):
+
+        return self._nombre
+
+    def setNombre(self,nombre):
+
+        self._nombre=nombre
+
+    def getUbicacion(self):
+         return self._ubicacion
+        
